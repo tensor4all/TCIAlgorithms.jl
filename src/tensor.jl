0e07542a574f1a1685559ab879f961b40c595eef
Ritter.Marc <Ritter.Marc@physik.uni-muenchen.de>
2023-06-16 19:04:59 +0200

    deltaproduct method for elementwise product of tensors in certain indices

--- src/tensor.jl
+++ src/tensor.jl
@@ -1,20 +1,46 @@
+function reshapetomatrix(A::AbstractArray, firstlegs::Union{AbstractVector{Int}, Tuple})
+    lastlegs = setdiff(1:ndims(A), firstlegs)
+    permutation = vcat(firstlegs, lastlegs)
+    Asizes = (size(A)[firstlegs], size(A)[lastlegs])
+    matrixdimensions = (prod(Asizes[1]), prod(Asizes[2]))
+    return reshape(permutedims(A, permutation), matrixdimensions), Asizes[2]
+end
 
 function contract(
-    A::AbstractArray, Alegs::AbstractVector{Int},
-    B::AbstractArray, Blegs::AbstractVector{Int}
+    A::AbstractArray, Alegs::Union{AbstractVector{Int}, Tuple},
+    B::AbstractArray, Blegs::Union{AbstractVector{Int}, Tuple}
 )
-    Alegsinv = setdiff(1:ndims(A), Alegs)
-    Aperm = vcat(Alegsinv, Alegs)
-    Adims = (prod(size(A)[Alegsinv]), prod(size(A)[Alegs]))
-
-    Blegsinv = setdiff(1:ndims(B), Blegs)
-    Bperm = vcat(Blegs, Blegsinv)
-    Bdims = (prod(size(B)[Blegs]), prod(size(B)[Blegsinv]))
-
-    result = reshape(permutedims(A, Aperm), Adims) * reshape(permutedims(B, Bperm), Bdims)
-    return reshape(result, (size(A)[Alegsinv]..., size(B)[Blegsinv]...))
+    Amatrix, Alegdims = reshapetomatrix(A, Alegs)
+    Bmatrix, Blegdims = reshapetomatrix(B, Blegs)
+    result = transpose(Amatrix) * Bmatrix
+    return reshape(result, Alegdims..., Blegdims...)
 end
 
 function contract(A::AbstractArray, Alegs::Int, B::AbstractArray, Blegs::Int)
     return contract(A, [Alegs], B, [Blegs])
+end
+
+"""
+Elementwise product in one index:
+``C_{ijk} = A_{ij} B_{jk}``
+"""
+function deltaproduct(A::AbstractMatrix{T}, B::AbstractMatrix{T}) where {T}
+    if size(A, 2) != size(B, 1)
+        throw(DimensionMismatch("Second dimension of A must have same length as first dimension of B."))
+    end
+    C = Array{T, 3}(undef, size(A, 1), size(A, 2), size(B, 2))
+    for j in axes(A, 2)
+        C[:, j, :] = A[:, j] * transpose(B[j, :])
+    end
+    return C
+end
+
+function deltaproduct(
+    A::AbstractArray, Alegs::Union{AbstractVector{Int}, Tuple},
+    B::AbstractArray, Blegs::Union{AbstractVector{Int}, Tuple}
+)
+    Amatrix, Alegdims = reshapetomatrix(A, Alegs)
+    Bmatrix, Blegdims = reshapetomatrix(B, Blegs)
+    result = deltaproduct(transpose(Amatrix), Bmatrix)
+    return reshape(result, Alegdims..., size(A)[Alegs]..., Blegdims...)
 end
--- test/test_tensor.jl
+++ test/test_tensor.jl
@@ -1,6 +1,6 @@
 using Test
 using LinearAlgebra
-import TCIAlgorithms: contract
+import TCIAlgorithms: contract, deltaproduct
 
 @testset "Tensor contraction" begin
     A = rand(4, 5)
@@ -14,4 +14,11 @@ import TCIAlgorithms: contract
     @test contract(C, [1], diagm([1, 1, 1]), [2]) ≈ permutedims(C, [2, 3, 4, 1])
     @test_throws DimensionMismatch contract(C, [2], diagm([1, 1, 1]), [2])
     @test contract(C, [4], diagm([0.5, 0.5, 0.5, 0.5]), [2]) == C ./ 2
+end
+
+@testset "Tensor elementwise multiplication" begin
+    A = rand(4, 5)
+    B = rand(4, 5)
+    @test deltaproduct(A, [1, 2], B, [1, 2]) ≈ A .* B
+    @test sum(deltaproduct(A, [1], B, [1]), dims=2)[:, 1, :] ≈ transpose(A) * B
 end
