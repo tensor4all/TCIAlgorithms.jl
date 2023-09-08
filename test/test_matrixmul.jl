4c06ea060b338efe0a04b04faa2b056b7aa0ecda
Ritter.Marc <Ritter.Marc@physik.uni-muenchen.de>
2023-09-08 11:42:47 +0200

    added comparison with ITensor to matrixmul test

--- test/test_matrixmul.jl
+++ test/test_matrixmul.jl
@@ -3,6 +3,7 @@ using LinearAlgebra
 import TensorCrossInterpolation as TCI
 import TCIAlgorithms: MatrixProduct
 import TCIAlgorithms as TCIA
+using TCIITensorConversion
 
 using ITensors
 
@@ -198,14 +199,32 @@ end
     localdims2 = [3, 3, 3, 3]
     localdims3 = [2, 2, 2, 2]
 
-    a = TCI.TensorTrain{ComplexF64, 4}([
+    a = TCI.TensorTrain{ComplexF64,4}([
         rand(ComplexF64, bonddims_a[n], localdims1[n], localdims2[n], bonddims_a[n+1]) for n = 1:N
     ])
-    b = TCI.TensorTrain{ComplexF64, 4}([
+    b = TCI.TensorTrain{ComplexF64,4}([
         rand(ComplexF64, bonddims_b[n], localdims2[n], localdims3[n], bonddims_b[n+1]) for n = 1:N
     ])
 
     ab = TCIA.contract(a, b)
     @test TCI.sitedims(ab) == [[localdims1[i], localdims3[i]] for i in 1:N]
-    @test all(TCI.linkdims(ab) .<= (bonddims_a .* bonddims_b)[2:end-1])
+
+    sites1 = Index.(localdims1, "1")
+    sites2 = Index.(localdims2, "2")
+    sites3 = Index.(localdims3, "3")
+
+    amps = MPO(a, sites=collect(zip(sites1, sites2)))
+    bmps = MPO(b, sites=collect(zip(sites2, sites3)))
+    abmps = amps * bmps
+
+    for inds1 in CartesianIndices(Tuple(localdims1))
+        for inds3 in CartesianIndices(Tuple(localdims3))
+            refvalue = evaluate_mps(
+                abmps,
+                collect(zip(sites1, Tuple(inds1))),
+                collect(zip(sites3, Tuple(inds3))))
+            inds = collect(zip(Tuple(inds1), Tuple(inds3)))
+            @test ab(inds) ≈ refvalue
+        end
+    end
 end
