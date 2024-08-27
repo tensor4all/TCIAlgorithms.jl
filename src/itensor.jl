98a7a04bdad9ed2b53ae2c1ca0a1da0b0dd7b811
Samuel Badr <samuel.badr@gmail.com>
2024-08-27 08:00:18 +0200

    finished implementation

--- src/itensor.jl
+++ src/itensor.jl
@@ -1,13 +1,9 @@
+# Struct Definitions
 struct ProjMPS
     data::MPS
     sites::Vector{Vector{Index}}
     projector::Projector
 
-    """
-    Constructor for ProjMPS.
-    The underlying memory layout of the data is permuted to match the order of the site indices.
-    The data may be copied.
-    """
     function ProjMPS(
         data::MPS, sites::AbstractVector{<:AbstractVector}, projector::Projector
     )
@@ -18,43 +14,42 @@ struct ProjMPS
     end
 end
 
+struct ProjMPSContainer
+    data::Vector{ProjMPS}
+    sites::Vector{Vector{Index}}
+    projector::Projector
+
+    function ProjMPSContainer(data::AbstractVector{ProjMPS})
+        for n in 2:length(data)
+            data[n].sites == data[1].sites ||
+                error("Sitedims mismatch $(data[n].sites) != $(data[1].sites)")
+        end
+        projector = reduce(|, x.projector for x in data)
+        return new(data, data[1].sites, projector)
+    end
+end
+
+# Constructor Functions
 function ProjMPS(Ψ::MPS, sites::AbstractVector{<:AbstractVector})
     sitedims = [collect(dim.(s)) for s in sites]
     globalprojector = Projector([fill(0, length(s)) for s in sitedims], sitedims)
     return ProjMPS(Ψ, sites, globalprojector)
 end
 
-function Base.show(io::IO, obj::ProjMPS)
-    return print(io, "ProjMPS projected on $(obj.projector.data)")
+function ProjMPSContainer(::Type{T}, projttcont::ProjTTContainer{T}, sites) where {T}
+    return ProjMPSContainer([ProjMPS(T, patch, sites) for patch in projttcont.data])
 end
 
-#_permdims(tensor::ITensor, inds...) = ITensor(Array(tensor, inds...), inds...)
-#_permdims(tensor::ITensor, inds::AbstractVector) = _permdims(tensor, inds...)
-
-function permutesiteinds(Ψ::MPS, sites::AbstractVector{<:AbstractVector})
-    links = linkinds(Ψ)
-    tensors = Vector{ITensor}(undef, length(Ψ))
-    tensors[1] = permute(Ψ[1], vcat(sites[1], links[1]))
-    for n in 2:(length(Ψ) - 1)
-        tensors[n] = permute(Ψ[n], vcat(links[n - 1], sites[n], links[n]))
-    end
-    tensors[end] = permute(Ψ[end], vcat(links[end], sites[end]))
-    return MPS(tensors)
-end
-
-# Conversion from ProjMPS to MPS
+# Conversion Functions
 ITensors.MPS(projΨ::ProjMPS) = projΨ.data
 
-# Conversion from ProjMPS to ProjTensorTrain
 function ProjTensorTrain{T}(projΨ::ProjMPS) where {T}
     return ProjTensorTrain{T}(
         asTT3(T, projΨ.data, projΨ.sites; permdims=false), projΨ.projector
     )
 end
 
-# Conversion from ProjTensorTrain to ProjMPS
 function ProjMPS(::Type{T}, projtt::ProjTensorTrain{T}, sites) where {T}
-    # To be implemented
     links = [Index(ld, "Link,l=$l") for (l, ld) in enumerate(TCI.linkdims(projtt.data))]
 
     tensors = ITensor[]
@@ -94,7 +89,21 @@ function ProjMPS(::Type{T}, projtt::ProjTensorTrain{T}, sites) where {T}
     return ProjMPS(MPS(tensors), sites, projtt.projector)
 end
 
-Base.isapprox(x::ProjMPS, y::ProjMPS; kwargs...) = Base.isapprox(x.data, y.data, kwargs...)
+function ProjTTContainer{T}(projmpss::ProjMPSContainer) where {T}
+    return ProjTTContainer([ProjTensorTrain{T}(projmps) for projmps in projmpss.data])
+end
+
+# Utility Functions
+function permutesiteinds(Ψ::MPS, sites::AbstractVector{<:AbstractVector})
+    links = linkinds(Ψ)
+    tensors = Vector{ITensor}(undef, length(Ψ))
+    tensors[1] = permute(Ψ[1], vcat(sites[1], links[1]))
+    for n in 2:(length(Ψ) - 1)
+        tensors[n] = permute(Ψ[n], vcat(links[n - 1], sites[n], links[n]))
+    end
+    tensors[end] = permute(Ψ[end], vcat(links[end], sites[end]))
+    return MPS(tensors)
+end
 
 function project(tensor::ITensor, projsiteinds::Dict{K,Int}) where {K}
     slice = Union{Int,Colon}[
@@ -104,16 +113,6 @@ function project(tensor::ITensor, projsiteinds::Dict{K,Int}) where {K}
     data_trim = zero(data_org)
     data_trim[slice...] .= data_org[slice...]
     return ITensor(data_trim, inds(tensor)...)
-end
-
-function find_nested_index(data::Vector{Vector{T}}, target::T) where {T}
-    for (i, subvector) in enumerate(data)
-        j = findfirst(x -> x == target, subvector)
-        if j !== nothing
-            return (i, j)
-        end
-    end
-    return nothing  # Not found
 end
 
 function project(oldprojector::Projector, sites, projsiteinds::Dict{Index{T},Int}) where {T}
@@ -145,7 +144,7 @@ function asTT3(::Type{T}, Ψ::MPS, sites; permdims=true)::TensorTrain{T,3} where
         push!(
             tensors,
             reshape(
-                Array(Ψ2[n], links[n - 1], sites[n]..., llinks[n]),
+                Array(Ψ2[n], links[n - 1], sites[n]..., links[n]),
                 dim(links[n - 1]),
                 :,
                 dim(links[n]),
@@ -199,49 +198,19 @@ function _check_projector_compatibility(
     )
 end
 
-struct ProjMPSContainer
-    # The projectors can overlap with each other.
-    data::Vector{ProjMPS}
-
-    # The site indices of the MPSs in `data`
-    # The order of site index vectors in `sites` does not necessarily match the order of the MPSs in `data`.
-    sites::Vector{Vector{Index}}
-
-    projector::Projector
-
-    function ProjMPSContainer(data::AbstractVector{ProjMPS})
-        for n in 2:length(data)
-            data[n].sites == data[1].sites ||
-                error("Sitedims mismatch $(data[n].sites) != $(data[1].sites)")
-        end
-        projector = reduce(|, x.projector for x in data)
-        return new(data, data[1].sites, projector)
-    end
-end
-
-# TODO: do we want to extend Quantics.rearrange_siteinds here?
-function rearrange_siteinds(projmps::ProjMPS, sites)
-    mps_rearranged = Quantics.rearrange_siteinds(projmps.data, sites)
-    projmps_rearranged = ProjMPS(mps_rearranged, sites)
-    prjsiteinds = Dict{Index{Int},Int}()
-    for (p, s) in zip(projmps.projector, projmps.sites)
-        for (p_, s_) in zip(p, s)
-            if p_ != 0
-                prjsiteinds[s_] = p_
-            end
+function find_nested_index(data::Vector{Vector{T}}, target::T) where {T}
+    for (i, subvector) in enumerate(data)
+        j = findfirst(x -> x == target, subvector)
+        if j !== nothing
+            return (i, j)
         end
     end
-    return project(projmps_rearranged, prjsiteinds)
+    return nothing  # Not found
 end
 
-function rearrange_siteinds(projmpss::ProjMPSContainer, sites)
-    return ProjMPSContainer([
-        rearrange_siteinds(projmps, sites) for projmps in projmpss.data
-    ])
-end
-
+# Quantics Functions
 function Quantics.makesitediagonal(projmps::ProjMPS, site::Index)
-    mps_diagonal = Quantics.makesitediagonal(projmps.data, site)
+    mps_diagonal = Quantics.makesitediagonal(MPS(projmps), site)
     sites_diagonal = siteinds(all, mps_diagonal)
     projmps_diagonal = ProjMPS(mps_diagonal, sites_diagonal)
 
@@ -259,12 +228,12 @@ function Quantics.makesitediagonal(projmps::ProjMPS, site::Index)
     return project(projmps_diagonal, prjsiteinds)
 end
 
-function makesitediagonal(projmps::ProjMPS, tag::String)
-    mps_diagonal = Quantics.makesitediagonal(projmps.data, tag)
+function Quantics.makesitediagonal(projmps::ProjMPS, tag::String)
+    mps_diagonal = Quantics.makesitediagonal(MPS(projmps), tag)
     sites_diagonal = siteinds(all, mps_diagonal)
     projmps_diagonal = ProjMPS(mps_diagonal, sites_diagonal)
 
-    target_positions = Quantics.findallsiteinds_by_tag(siteinds(projmps.data); tag=tag)
+    target_positions = Quantics.findallsiteinds_by_tag(siteinds(MPS(projmps)); tag=tag)
     prjsiteinds = Dict{Index{Int},Int}()
     for (p, s) in zip(projmps.projector, projmps.sites)
         for (p_, s_) in zip(p, s)
@@ -279,10 +248,74 @@ function makesitediagonal(projmps::ProjMPS, tag::String)
     return project(projmps_diagonal, prjsiteinds)
 end
 
-function makesitediagonal(projmpss::ProjMPSContainer, sites)
-    return ProjMPSContainer([makesitediagonal(projmps, sites) for projmps in projmpss.data])
+function Quantics.makesitediagonal(projmpss::ProjMPSContainer, sites)
+    return ProjMPSContainer([
+        Quantics.makesitediagonal(projmps, sites) for projmps in projmpss.data
+    ])
 end
 
+function Quantics.extractdiagonal(projmps::ProjMPS, tag::String)
+    mps_diagonal = Quantics.extractdiagonal(MPS(projmps), tag)
+    sites_diagonal = siteinds(all, mps_diagonal)
+    projmps_diagonal = ProjMPS(mps_diagonal, sites_diagonal)
+    sites_diagonal_set = Set(Iterators.flatten(sites_diagonal))
+
+    prjsiteinds = Dict{Index{Int},Int}()
+    for (p, s) in zip(projmps.projector, projmps.sites)
+        for (p_, s_) in zip(p, s)
+            !iszero(p_) || continue
+            s_ ∈ sites_diagonal_set || continue
+            prjsiteinds[s_] = p_
+        end
+    end
+
+    return project(projmps_diagonal, prjsiteinds)
+end
+
+function Quantics.extractdiagonal(projmpss::ProjMPSContainer, sites)
+    return ProjMPSContainer([
+        Quantics.extractdiagonal(projmps, sites) for projmps in projmpss.data
+    ])
+end
+
+function Quantics.rearrange_siteinds(projmps::ProjMPS, sites)
+    mps_rearranged = Quantics.rearrange_siteinds(MPS(projmps), sites)
+    projmps_rearranged = ProjMPS(mps_rearranged, sites)
+    prjsiteinds = Dict{Index{Int},Int}()
+    for (p, s) in zip(projmps.projector, projmps.sites)
+        for (p_, s_) in zip(p, s)
+            if p_ != 0
+                prjsiteinds[s_] = p_
+            end
+        end
+    end
+    return project(projmps_rearranged, prjsiteinds)
+end
+
+function Quantics.rearrange_siteinds(projmpss::ProjMPSContainer, sites)
+    return ProjMPSContainer([
+        Quantics.rearrange_siteinds(projmps, sites) for projmps in projmpss.data
+    ])
+end
+
+# Miscellaneous Functions
+function Base.show(io::IO, obj::ProjMPS)
+    return print(io, "ProjMPS projected on $(obj.projector.data)")
+end
+
+function ITensors.prime(Ψ::ProjMPS, args...; kwargs...)
+    return ProjMPS(
+        prime(MPS(Ψ), args...; kwargs...), prime.(Ψ.sites, args...; kwargs...), Ψ.projector
+    )
+end
+
+function ITensors.prime(Ψ::ProjMPSContainer, args...; kwargs...)
+    return ProjMPSContainer([prime(projmps, args...; kwargs...) for projmps in Ψ.data])
+end
+
+Base.isapprox(x::ProjMPS, y::ProjMPS; kwargs...) = Base.isapprox(x.data, y.data, kwargs...)
+
+# Random MPO Functions (commented out)
 #==
 function _random_mpo(
     rng::AbstractRNG, sites::AbstractVector{<:AbstractVector{Index{T}}}; m::Int=1
@@ -302,7 +335,3 @@ function _random_mpo(sites::AbstractVector{<:AbstractVector{Index{T}}}; m::Int=1
     return _random_mpo(Random.default_rng(), sites; m=m)
 end
 ==#
-
-# Wrappers for
-# matmul()
-# adaptivematmul()
